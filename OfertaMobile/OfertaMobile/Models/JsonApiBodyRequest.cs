88e814c419be925d479555f5090e96a697a296dc
Santiago Acevedo <santiago.acevedo@accenture.com>
2018-09-19 09:00:27 -0500

    first commit

---
+++ OfertaMobile/OfertaMobile/Models/JsonApiBodyRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfertaMobile.Models
+{
+    class JsonApiBodyRequest
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+        public string correo { get; set; }
+        public string contrasena { get; set; }
+        public string telefono { get; set; }
+        public string genero { get; set; }
+        public string rol { get; set; }
+        public string ciudad { get; set; }
+        public string estado { get; set; }
+        public string token { get; set; }
+
+        public JsonApiBodyRequest() {}
+
+    }
+}
